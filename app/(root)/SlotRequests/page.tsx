105327db65ea34d1a070de5a633baae0466e51f3
NitinS47 <154044708+NitinS47@users.noreply.github.com>
2025-02-19 12:27:38 +0530

    Update page.tsx

--- app/(root)/SlotRequests/page.tsx
+++ app/(root)/SlotRequests/page.tsx
@@ -1,13 +1,26 @@
-import SlotRequestsTable from '@/components/ui/SlotsRequestTable'
-import React from 'react'
+"use client";
 
-const SlotRequestsPage = () => {
+import React from "react";
+import dynamic from "next/dynamic";
+import TableApp from "../../../components/ui/TableApp";
+
+// Import Navbar dynamically with no SSR
+const Navbar = dynamic(() => import("@/components/Navbar"), {
+  ssr: false,
+  loading: () => (
+    <div className="h-[64px] w-full bg-background/60 backdrop-blur-lg" />
+  ),
+});
+
+const ApprovalPage = () => {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Slot Requests Management</h1>
-      <SlotRequestsTable />
-    </div>
-  )
-}
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="w-full">
+        <Navbar aria-label="Main Navigation" />
+        <TableApp  />
+      </div>
+    </main>
+  );
+};
 
-export default SlotRequestsPage
+export default ApprovalPage;
