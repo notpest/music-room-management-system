4389cbd7cdc592d2f2a078e02cbab04020d87287
notpest <rejoy.cherian@btech.christuniversity.in>
2025-02-19 12:26:27 +0530

    Merge branch 'master' of https://github.com/notpest/music-room-management-system

--- app/(root)/RoomBooking/page.tsx
+++ app/(root)/RoomBooking/page.tsx
@@ -2,9 +2,25 @@
 
 import React from "react";
 import RBTable from "../../../components/ui/RBTable";
+import dynamic from "next/dynamic";
+
+// Import Navbar dynamically with no SSR
+const Navbar = dynamic(() => import("@/components/Navbar"), {
+  ssr: false,
+  loading: () => (
+    <div className="h-[64px] w-full bg-background/60 backdrop-blur-lg" />
+  ),
+});
 
 const RoomBookingPage = () => {
-  return <RBTable />;
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="w-full">
+        <Navbar aria-label="Main Navigation" />
+        <RBTable />
+      </div>
+    </main>
+  );
 };
 
 export default RoomBookingPage;
