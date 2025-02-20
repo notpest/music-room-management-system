886941ad27f445ff2e5cbf6501679b5679d67827
notpest <rejoy.cherian@btech.christuniversity.in>
2025-02-20 14:55:41 +0530

    slotrequests fix

--- app/(root)/SlotRequests/page.tsx
+++ app/(root)/SlotRequests/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
-import TableApp from "../../../components/ui/TableApp";
+import SlotsRequestsTable from "../../../components/ui/SlotsRequestTable";
 
 // Import Navbar dynamically with no SSR
 const Navbar = dynamic(() => import("@/components/Navbar"), {
@@ -12,17 +12,17 @@ const Navbar = dynamic(() => import("@/components/Navbar"), {
   ),
 });
 
-const SlotRequests = () => {
+const SlotRequestsPage = () => {
   return (
     <div className="bg-black-100">
       <Navbar aria-label="Main Navigation" />
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="w-full">
-        <TableApp  />
+        <SlotsRequestsTable  />
       </div>
     </main>
     </div>
   );
 };
 
-export default SlotRequests;
+export default SlotRequestsPage;
