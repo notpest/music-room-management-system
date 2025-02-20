26a84d1d1b98059b3acd05c30855ee86d4dbcec3
NitinS47 <154044708+NitinS47@users.noreply.github.com>
2025-02-20 14:18:11 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -164,6 +164,11 @@ const NavbarComponent = () => {
               label="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleLogin();
+                }
+              }}
             />
           </ModalBody>
           <ModalFooter>
