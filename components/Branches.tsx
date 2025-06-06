c109c306eaeb074d7a178f9ec6ef53dd969955e3
NitinS47 <154044708+NitinS47@users.noreply.github.com>
2025-06-06 09:50:51 +0530

    branches

--- components/Branches.tsx
+++ components/Branches.tsx
@@ -7,18 +7,18 @@ import ckc from "../public/Choir/CKC.png";
 const Branches = () => {
   const cards = [
     {
-      title: "The Volunteer Body",
-      desc: "We volunteer man",
+      title: "The Cultural Team",
+      desc: "Mentored by Mr. Jobin Shiji Daniel, they are the face of the University and participate in fests all across India. The Team consists of students from every department, and are the best of the best in their own fields.",
       src: SWO_Logo, // Use imported image directly
     },
     {
       title: "The University Choir",
-      desc: "We sing ;)",
+      desc: "Guided by Mr. Roshan Yohann Anand, they are one of Bangalore's premier performing choirs with over 120 members. They're well known for their annual Christmas Concert Magnificat as well as their guest performances at AIVSC and Reound (SJCC).",
       src: ckc,
     },
     {
       title: "Natyarpana",
-      desc: "We Dance Nicely",
+      desc: "The University Dance Team, guided by Mrs. Nivedya Don, is a group of dancing talents handpicked from the University. They are most prominently known for the University Dance Day - Nritta, being the celebration of dance in all forms.",
       src: Natyarpana,
     },
      ];
